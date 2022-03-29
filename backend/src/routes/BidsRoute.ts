203970fd2d30c2f93503bdf866de751049a4b2f0
vprasa03 <vprasa03@student.bbk.ac.uk>
2022-03-29 02:12:07 +0100

    Add /bids route

---
+++ backend/src/routes/BidsRoute.ts
@@ -0,0 +1,34 @@
+import { Router } from "express";
+
+/**
+ * Bids route ("/bids/")
+ * @public
+ */
+class BidsRoute {
+	private router: Router;
+
+	constructor() {
+		this.router = Router();
+
+		this.getHome();
+	}
+
+	/**
+	 * GET "/bids"
+	 */
+	private getHome() {
+		this.router.get("/", (_req, res) => {
+			res.send("Bids!");
+		});
+	}
+
+	/**
+	 * Get bids router
+	 * @returns Router object
+	 */
+	public getRouter() {
+		return this.router;
+	}
+}
+
+export const bidsRoute = new BidsRoute();
---
+++ backend/src/routes/index.ts
@@ -0,0 +1,2 @@
+export * from "./BidsRoute";
+export * from "./RootRoute";
