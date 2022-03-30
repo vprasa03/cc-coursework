5869f6224d8bd8f5066173e1532f862d9fa6ae23
vprasa03 <vprasa03@student.bbk.ac.uk>
2022-03-30 01:55:42 +0100

    Add db connect

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,29 @@
+import { connect } from "mongoose";
 import dotenv = require("dotenv");
 
 import { App } from "./App";
 import { bidsRoute, rootRoute } from "./routes";
+import { isNativeError } from "util/types";
 
 const envVars = dotenv.config();
-console.log(envVars.parsed?.MURL);
 
-App.use("/", rootRoute.getRouter());
-App.use("/bids", bidsRoute.getRouter());
+function onDBConnect() {
+	App.use("/", rootRoute.getRouter());
+	App.use("/bids", bidsRoute.getRouter());
 
-App.start(3000);
+	App.start(3000);
+}
+
+function connectDB(successCB: () => void) {
+	try {
+		connect(envVars.parsed?.MURL, (error) => {
+			if (isNativeError(error)) throw error;
+			successCB();
+		});
+	} catch (error) {
+		console.error(error, "\nRetry DB connection...");
+		connectDB(successCB);
+	}
+}
+
+connectDB(onDBConnect);
