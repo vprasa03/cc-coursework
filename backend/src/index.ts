51f1c76c8208f3e88f0fe26de11de5a7128a78b1
vprasa03 <vprasa03@student.bbk.ac.uk>
2022-03-31 20:44:09 +0100

    Add MURL env variable not found check

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import bodyParser = require("body-parser");
 import { isNativeError } from "util/types";
 
 import { App } from "./App";
-import { auctionsRoute, userRoute } from "./routes";
+import { auctionRoute, auctionsRoute, userRoute } from "./routes";
 
 const envVars = dotenv.config();
 
@@ -12,16 +12,23 @@ function onDBConnect() {
 	App.useMiddleware(bodyParser.json());
 	App.useRoute("/user", userRoute.getRouter());
 	App.useRoute("/auctions", auctionsRoute.getRouter());
+	App.useRoute("/auction", auctionRoute.getRouter());
 
 	App.start(3000);
 }
 
+const dbUrl = envVars.parsed?.MURL;
+
 function connectDB(successCB: () => void) {
 	try {
-		connect(envVars.parsed?.MURL, (error) => {
-			if (isNativeError(error)) throw error;
-			successCB();
-		});
+		if (dbUrl) {
+			connect(dbUrl, (error) => {
+				if (isNativeError(error)) throw error;
+				successCB();
+			});
+		} else {
+			throw new Error("DB url not found!");
+		}
 	} catch (error) {
 		console.error(error, "\nRetry DB connection...");
 		connectDB(successCB);
